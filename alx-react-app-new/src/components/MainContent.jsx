52ce95e56365987897e272ecdcf850fba8a6de61
OluwadamilolaOlaniyan <oluwadamilola.olaniyan@gmail.com>
2026-01-18 03:57:49 +0100

    created react new app

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,15 @@
+function MainContent() {
+  return (
+    <main
+      style={{
+        padding: '20px',
+        fontSize: '18px',
+        backgroundColor: '#f4f4f4'
+      }}
+    >
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
