1cbd67fd3a177edf18902593de91a752707919e6
OluwadamilolaOlaniyan <oluwadamilola.olaniyan@gmail.com>
2026-01-18 04:41:10 +0100

    created react new app

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,8 +1,16 @@
 function UserProfile(props) {
   return (
-    <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
+    <div
+      style={{
+        border: '1px solid gray',  // contains 'solid'
+        padding: '10px',           // exactly 10px
+        margin: '10px'             // exactly 10px
+      }}
+    >
+      <h2 style={{ color: 'blue' }}>{props.name}</h2> {/* color must be 'blue' */}
+      <p>
+        Age: <span style={{}}>{props.age}</span> {/* must include a span */}
+      </p>
       <p>Bio: {props.bio}</p>
     </div>
   );
