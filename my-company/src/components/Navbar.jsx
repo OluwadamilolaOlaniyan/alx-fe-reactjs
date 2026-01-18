df9606d79731330c6b2f4e7989b8c8c85c199d13
OluwadamilolaOlaniyan <oluwadamilola.olaniyan@gmail.com>
2026-01-18 06:15:30 +0100

    navbar updated in component folder

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,11 +2,20 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ padding: "10px", backgroundColor: "#333", color: "white" }}>
-      <Link to="/" style={{ margin: "10px", color: "white" }}>Home</Link>
-      <Link to="/about" style={{ margin: "10px", color: "white" }}>About</Link>
-      <Link to="/services" style={{ margin: "10px", color: "white" }}>Services</Link>
-      <Link to="/contact" style={{ margin: "10px", color: "white" }}>Contact</Link>
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "10px",
+        backgroundColor: "#333",
+        color: "white",
+      }}
+    >
+      <Link to="/" style={{ margin: "0 15px", color: "white", textDecoration: "none" }}>Home</Link>
+      <Link to="/about" style={{ margin: "0 15px", color: "white", textDecoration: "none" }}>About</Link>
+      <Link to="/services" style={{ margin: "0 15px", color: "white", textDecoration: "none" }}>Services</Link>
+      <Link to="/contact" style={{ margin: "0 15px", color: "white", textDecoration: "none" }}>Contact</Link>
     </nav>
   );
 }
