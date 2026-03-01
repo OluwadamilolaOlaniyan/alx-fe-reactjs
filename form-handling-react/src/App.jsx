6d5c008e5761f29a2595e843d7c05b223b33c4d8
OluwadamilolaOlaniyan <oluwadamilola.olaniyan@gmail.com>
2026-03-01 20:14:51 +0100

    updated Registration Forms

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -3,14 +3,11 @@ import FormikForm from "./components/FormikForm";
 
 function App() {
   return (
-    <div style={{ padding: "40px", fontFamily: "Arial" }}>
-      <div style={{ marginBottom: "60px" }}>
-        <RegistrationForm />
-      </div>
-
-      <div>
-        <FormikForm />
-      </div>
+    <div>
+      <RegistrationForm />
+      <FormikForm />
     </div>
   );
 }
+
+export default App;
